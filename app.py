639ad5b614bd2ad7eeeaf83ca877e92fa149a1a8
Elouan <138057418+elouan-cottin@users.noreply.github.com>
2025-04-01 21:04:11 +0300

    Created absis our first application
    it includes mathematical applications and manipulation data types

---
+++ app.py
@@ -0,0 +1,29 @@
+#Simple Python
+x =150
+y=“Marceille"
+name = "anna"
+surname = "Grey"
+student_name = name + " " + surname
+#Python is using indent
+a = 10
+b = 5
+c = a ** b
+d = a / b
+e = a * b
+f = a % b
+
+
+#a == b 
+#a || b
+#a === b
+#a != b
+#a ^^ b 
+#a && b
+
+print(x)
+print(y)
+print(student_name)
+print(c)
+print(d)
+print(e)
+print(f)
